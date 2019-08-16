f147d94c505ff8344ff72d43449efd4ca21d877e
khanhphamqn <khanhphamqn@gmail.com>
2019-08-16 23:05:11 +0700

    finish code

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# khanhphamqnreact-native-login-google-example
+# react-native-login-google-example
 an example demonstrates how to integrate google login on react native
+#use 
+Google Sign-In SDK 4.4.0
+"react": "16.8.3",
+"react-native": "0.59.9",
+"react-native-google-signin": "^2.0.0"
--- ios/initProject/AppDelegate.m
+++ ios/initProject/AppDelegate.m
@@ -10,6 +10,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import "RNGoogleSignin.h"
 
 @implementation AppDelegate
 
@@ -37,6 +38,13 @@
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
+}
+
+- (BOOL)application:(UIApplication *)application openURL:(nonnull NSURL *)url options:(nonnull NSDictionary<NSString *,id> *)options {
+  return [RNGoogleSignin application:application
+                             openURL:url
+                   sourceApplication:options[UIApplicationOpenURLOptionsSourceApplicationKey]
+                          annotation:options[UIApplicationOpenURLOptionsAnnotationKey]];
 }
 
 @end
